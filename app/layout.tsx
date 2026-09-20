2ce34b3d7c175c0f0988ecf00c3e2ec62fc56723
Bismaparveen <bismaparveen655@gmail.com>
2026-09-20 21:19:49 +0500

    fix the build issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
-    </html>
+    <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,500;0,600;1,400;1,500&family=DM+Sans:wght@400;450;500;600&display=swap"
+        rel="stylesheet"
+      />
+    </head>
+    <body className="antialiased">{children}</body>
+  </html>
   );
 }
